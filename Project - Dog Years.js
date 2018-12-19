c319049d49e551e1b4d90ee12a1d6d8df0a49941
Francisco Mateus <gtaskfm@gmail.com>
2018-12-19 21:40:11 +0100

    Create Project - Dog Years.js

---
+++ Project - Dog Years.js
@@ -0,0 +1,27 @@
+var myAge = 26;
+// variable defined as number 26;
+
+let earlyYears = 2;
+// var as let since will change, val to two
+
+earlyYears *= 10.5;
+// multiplied the value by 10.5
+
+let laterYears = myAge - 2;
+// Changable let
+
+laterYears *= 4;
+// multiplied the value by 4
+
+console.log(earlyYears);
+console.log(laterYears);
+
+const myAgeInDogYears = earlyYears + laterYears;
+// my Age in dog years
+
+var myName = 'Francisco Mateus';
+myName.toLowerCase();
+// get my name only to lowercase letters
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
