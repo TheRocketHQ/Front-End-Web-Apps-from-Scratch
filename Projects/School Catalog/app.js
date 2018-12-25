45485a897249ec5c24ab5a0736c304e368326d2e
Francisco Mateus <gtaskfm@gmail.com>
2018-12-25 11:39:05 +0100

    class School

--- Projects/School Catalog/app.js
+++ Projects/School Catalog/app.js
@@ -4,5 +4,34 @@ class School {
         this._level = level;
         this._numberOfStudents = numberOfStudents;
     }
-     
+    
+    get name(){
+        return this._name;
+    }
+
+    get level(){
+        return this._level;
+    }
+
+    get numberOfStudents(){
+        return this._numberOfStudents;
+    }
+
+    set numberOfStudents(value){
+        if(value.isNan()){
+            console.log('Invalid Input: numberOfStudents must be a number.');
+        }
+        else{
+            this._numberOfStudents = value;
+        }
+    }
+
+    quickFacts(){
+        console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
+    }
+
+    static pickSubTeacher (substituteTeachers){
+        const randInt = Math.floor(Math.random()* substituteTeachers.length);
+        return substituteTeachers[randInt];
+    }
 }
