1cb5ffe8d8878e48196de04a16b87befb0d456d2
Francisco Mateus <gtaskfm@gmail.com>
2018-12-25 10:52:32 +0100

    Initial commit - created the class media

---
+++ Projects/Library/app.js
@@ -0,0 +1,34 @@
+class Media {
+    constructor (title) {
+        this._title = title;
+        this._isCheckedOut = false;
+        this._ratings = [];
+    }
+
+    get title (){
+        return this._title;
+    }
+    get isCheckedOut (){
+        return this._isCheckedOut;
+    }
+    get ratings (){
+        return this._ratings;
+    }
+
+    set isCheckedOut (value) {
+        this._isCheckedOut = value;
+    }
+
+    toggleCheckedOutStatus(){
+        this.isCheckedOut = !this.isCheckedOut;
+    }
+
+    getAverageRating(){
+        let ratingsSum = this.ratings.reduce((accumulator, rating) => accumulator + rating);
+        return ratingsSum/this.ratings.length;
+    }
+
+    addRating(value){
+        this.ratings.push(value);
+    }
+}
