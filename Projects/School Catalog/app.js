abe7ce2079d8e6bc6dc6e0c877039bc8c101a1be
Francisco Mateus <gtaskfm@gmail.com>
2018-12-25 11:30:33 +0100

    Initial Commit - School Catalog

---
+++ Projects/School Catalog/app.js
@@ -0,0 +1,8 @@
+class School {
+    constructor(name, level, numberOfStudents){
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+     
+}
