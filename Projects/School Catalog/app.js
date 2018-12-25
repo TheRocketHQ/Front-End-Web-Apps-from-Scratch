0c8324a793ca6abd6d5d77a8da9f5cfb4b9df295
Francisco Mateus <gtaskfm@gmail.com>
2018-12-25 11:53:53 +0100

    extended school with PrimarySchool and HighSchool

--- Projects/School Catalog/app.js
+++ Projects/School Catalog/app.js
@@ -35,3 +35,54 @@ class School {
         return substituteTeachers[randInt];
     }
 }
+
+class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickupPolicy){
+        super (name, 'primary', numberOfStudents);
+        this._pickupPolicy = pickupPolicy;
+    }
+
+    get pickupPolicy(){
+        return this._pickupPolicy;
+    }
+}
+
+class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams){
+        super (name, 'high', numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
+
+    get sportsTeams(){
+        return this._sportsTeams;
+    }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansburry', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+
+const sub = School.pickSubTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Volleyball', 'Track and Field']);
+console.log(alSmith.sportsTeams);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
