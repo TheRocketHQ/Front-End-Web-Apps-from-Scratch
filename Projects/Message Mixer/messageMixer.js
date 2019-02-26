77b679d4e7b8a0d92bacd6050e5ee6c840bb078c
RocketRacoon <gtaskfm@gmail.com>
2019-02-26 20:28:32 +0100

    Message
    Changed display message function

--- Projects/Message Mixer/messageMixer.js
+++ Projects/Message Mixer/messageMixer.js
@@ -1,3 +1,7 @@
+const MessageMixer = {};
+
+MessageMixer.
+
 function countCharacter(inputString, inputCharacter) {
     let count = 0;
     let string = inputString.toLowerCase();
@@ -51,13 +55,4 @@ function countCharacter(inputString, inputCharacter) {
   };
   
   
-  function displayMessage() {
-    console.log(countCharacter("What is the color of the sky?", "t"));
-    console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-    console.log(reverseWord("What is the color of the sky?"));
-    console.log(reverseAllWords("What is the color of the sky?"));
-    console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-    console.log(encode("What is the color of the sky?"));
-  }
-  
-  displayMessage();
+ 
