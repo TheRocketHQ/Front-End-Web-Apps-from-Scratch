3d63f1cd7046758f220b3d4f8cc6414b8b98cd5d
RocketRacoon <gtaskfm@gmail.com>
2019-03-05 14:50:29 +0100

    Child/parent/sibling Video Player
    We used:
    App as a parent it holds the menu and video it doenst hold value
    Menu as a sibling - it holds the value to change
    Video as a child - it displays the value

--- Projects/videoPlayer/Video.js
+++ Projects/videoPlayer/Video.js
@@ -4,7 +4,7 @@ export class Video extends React.Component {
   render() {
     return (
       <div>
-        <video controls autostart autoplay />
+        <video controls autostart autoplay src={this.props.src} />
       </div>
     );
   }
